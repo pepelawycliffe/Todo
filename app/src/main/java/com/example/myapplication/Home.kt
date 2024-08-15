edddfe1348a7d9a705658a110d8b3f636fe9e002
wycliffe <wycliffepepela01@gmail.com>
2024-08-15 16:28:01 +0300

    first commit

--- app/src/main/java/com/example/myapplication/Home.kt
+++ app/src/main/java/com/example/myapplication/Home.kt
@@ -1,24 +1,78 @@
 package com.example.myapplication
 
+import android.R.attr.onClick
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFrom
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults.elevation
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
 fun HomeScreen(navController: NavController) {
-    Column (modifier= Modifier.focusModifier()
-        .fillMaxSize().background(color =Color(0xFF6650a4)),
-        horizontalAlignment = Alignment.CenterHorizontally
-        ,verticalArrangement = Arrangement.Center,){
+
+    Column(modifier = Modifier
+        .background(color = Color(0xFF6650a4))
+        .fillMaxSize()) {
+        Column (modifier= Modifier.padding(35.dp)
+            , horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Column (modifier = Modifier.padding(5.dp),
+                verticalArrangement = Arrangement.Top
+                ,horizontalAlignment = Alignment.Start){
+                ElevatedCard (
+                    elevation = CardDefaults.cardElevation(
+                        defaultElevation = 4.dp
+                    ),modifier = Modifier
+                        .size(width = 420.dp, height = 100.dp)
+                ){
+                    Text(text = "Hello this is a card view",modifier = Modifier
+                        .padding(16.dp),
+                        textAlign = TextAlign.Center,)
+                }
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+        }
+        Column (modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp),
+            verticalArrangement = Arrangement.Bottom
+            ,horizontalAlignment = AbsoluteAlignment.Right) {
+            Column (modifier = Modifier
+                .padding(5.dp),
+                verticalArrangement = Arrangement.Top
+                ,horizontalAlignment = Alignment.Start){
+                ExtendedFloatingActionButton(
+                    onClick = { ""}) {
+                    Icon(Icons.Filled.Edit, "Large floating action button")
+                }
+
+            }
+        }
 
     }
+
 }
--- app/src/main/java/com/example/myapplication/Welcome.kt
+++ app/src/main/java/com/example/myapplication/Welcome.kt
@@ -5,8 +5,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -15,13 +19,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 @Composable
 fun WelcomeScreen(navController: NavController) {
- Column (modifier= Modifier.focusModifier()
+ Column (modifier= Modifier
   .fillMaxSize()
   .background(color = Color(0xFFEFB8C8)),
   horizontalAlignment = Alignment.CenterHorizontally
@@ -30,13 +35,22 @@ fun WelcomeScreen(navController: NavController) {
    fontSize = 28.sp,
    color = Color(0xFF6650a4)
   )
-
+  Spacer(modifier = Modifier.height(50.dp))
+ Column(modifier = Modifier,
+  verticalArrangement = Arrangement.Center,
+  horizontalAlignment = Alignment.CenterHorizontally) {
   Text(
    text = "Todo Plus is a TASK Manager that helps you organize your Task with the priorities",
+   textAlign = TextAlign.Center,
    fontSize = 21.sp,
   )
-  Column (modifier= Modifier.focusModifier(), horizontalAlignment = Alignment.End){
-   Button(onClick = { navController.navigate(Routes.HomeScreen)}) {
+ }
+  Spacer(modifier = Modifier.height(10.dp))
+  Column (modifier= Modifier.size(300.dp),
+   verticalArrangement = Arrangement.Bottom,
+   horizontalAlignment = Alignment.CenterHorizontally
+  ){
+   Button(onClick = { navController.navigate(Routes.HomeScreen)}, Modifier.size(width = 200.dp, height = 60.dp)) {
 
     Text(text = "GET STARTED")
    }
